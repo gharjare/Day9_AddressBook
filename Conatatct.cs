dca54d5d8d9dd69e06878b4059fc61e0da5a3c1f
gharjare <gharjarechetan@gmail.com>
2022-12-09 13:48:24 +0530

    create contacts

---
+++ Conatatct.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookAssignment
+{
+    public class Conatatct
+    {
+       public string FName { get; set; }
+        public string LName { get; set; }
+        public string Addresss { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int Zip { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+    }
+}
