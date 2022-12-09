1cbb671754d6a45f5f04fd90c8722de14bcec222
gharjare <gharjarechetan@gmail.com>
2022-12-09 13:46:29 +0530

    wlc msg

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace AddressBookAssignment
+{
+    class program
+    {
+       //  public static  List<Conatatct> person = new List<Conatatct>();
+       
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to AddressBook Program");
+
+             AddressBookAssignment.Person.CreateContact();
+
+           // Person.editContact();
+            Person.deleteContact();
+            
+        }
+    }
+}
